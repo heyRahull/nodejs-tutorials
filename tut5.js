a26858e8286331f18c704f9737a68ceb1e2bf8c2
Rahul Agarwal <rahul123agarwal99@gmail.com>
2026-06-06 22:04:00 +0530

    tut5 : Package.json & NPM Basics

---
+++ tut5.js
@@ -0,0 +1,12 @@
+// 1. Core Module - eg : fs, http, path, os, etc
+// 2. Custom Module : eg: utils.js
+// 3. Third Party module : eg: colors, express, etc
+
+var colors = require('colors');
+ 
+console.log('hello'.green); // outputs green text
+console.log('i like cake and pies'.underline.red) // outputs red underlined text
+console.log('inverse the color'.inverse); // inverses the color
+console.log('OMG Rainbows!'.rainbow); // rainbow
+console.log('Run the trap'.trap); // Drops the bass
+ 
